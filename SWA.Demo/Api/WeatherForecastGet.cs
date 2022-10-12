1a7d8b25eecef05322a532f75f95cdb627abf39b
Melissa Houghton <57604950+melissahoughton@users.noreply.github.com>
2022-10-12 12:30:46 +1100

    Add initial Blazor WASM + Azure Function SWA app

---
+++ SWA.Demo/Api/WeatherForecastGet.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+using System;
+using Data;
+
+namespace Api;
+
+public static class WeatherForecastGet
+{
+    [FunctionName("WeatherForecastGet")]
+    public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+    {
+        var randomNumber = new Random();
+        var temp = 0;
+
+        var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = temp = randomNumber.Next(-10, 35),
+            Summary = GetSummary(temp)
+        }).ToArray();
+
+        return new OkObjectResult(result);
+    }
+
+    private static string GetSummary(int temp)
+    {
+        var summary = "Mild";
+
+        if (temp >= 32)
+        {
+            summary = "Hot";
+        }
+        else if (temp <= 16 && temp > 0)
+        {
+            summary = "Cold";
+        }
+        else if (temp <= 0)
+        {
+            summary = "Freezing!";
+        }
+
+        return summary;
+    }
+}
